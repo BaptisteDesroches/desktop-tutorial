c0a4758ece18525fd390d0bdf1993481d5894ad3
BaptisteDesroches <137180255+BaptisteDesroches@users.noreply.github.com>
2023-11-19 15:29:51 +0100

    Add files via upload

--- example.php
+++ example.php
@@ -10,6 +10,15 @@
     </head>
 
     <body>
+    <nav class="box">
+                <img class="logo" alt="" src="assets\logo.JPG"/>
+                <ul>
+                    <li><a href="Actualités">Actualités</a></li>
+                    <li><a href="Jeux">Jeux</a></li>
+                </ul>
+                <input class="search" type="text" id="searchInput" placeholder="Search..." oninput="performSearch()">
+                <div id="searchResults"></div>
+            </nav>
 <?php
 $bdd = new PDO('mysql:host=localhost;dbname=games;charset=utf8', 'root', '');
 $reponse = $bdd->query('select * from games');
@@ -18,6 +27,7 @@ while ($donnees = $reponse->fetch())
 ?>
 
         <p><?php echo $donnees['title']; ?></p>
+        <p><?php echo $donnees['description']; ?></p>
 <?php
     }
     $reponse->closeCursor();//important
