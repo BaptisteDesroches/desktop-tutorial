8ebd8c9e2009998d5591928717393b10e98de5b4
BaptisteDesroches <137180255+BaptisteDesroches@users.noreply.github.com>
2023-11-20 14:57:20 +0100

    Update traitement.php

--- traitement.php
+++ traitement.php
@@ -4,16 +4,15 @@ $servername="localhost";
 $username ="root";
 $password ="root";
 
-$bdd = new PDO('mysql:host=localhost;dbname=userss;charset=utf8', 'root', '');
+$bdd = new PDO('mysql:host=localhost;dbname=users;charset=utf8', 'root', '');
 
 
-// récupérer la data insite
+// récupérer la data in-site
 if(isset($_POST['ok'])){
     $nom = $_POST['nom'];
 }
 
 //envoyer la data à la bdd
-
 $requete = $bdd->prepare("INSERT INTO users VALUES (0, :pseudo, :nom, :prenom, MD5:mdp, :email)";
 $requete-> execute(
 array(
